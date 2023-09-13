05d33d45dfbc48f180dfd7f785a2865feee422fd
felipe <machadofelipe2016@outlook.com>
2023-09-13 17:52:45 -0300

    at this moment trying to figured out the issues

--- index.js
+++ index.js
@@ -3,26 +3,27 @@ const express= require("express")
 const api = express()
 const redis = require("redis")
 
+
 // remind i have to always define my port to connect together with docker
 const client = redis.createClient({
-   host:"redis-server"
+   host:'redis-server',
+   port:6379
 })
 
-
+await client.connect()
 
 client.set('visits', 0)
 
 api.get('/', (req, res) => {    
     // put together something new with redis
     client.get('visits', (err,visits) =>{
-        res.send("the numbers of visits here is ", visits) 
-        
+        res.send("the numbers of visits here is ", visits)  
         client.set('clients', parseInt(client+1))
     })
 
 })
 
 
-api.listen(8081, () => {
-   console.log(`connected here now ${8081}`)
+api.listen(4001, () => {
+   console.log('connect here now 8081')
 })
